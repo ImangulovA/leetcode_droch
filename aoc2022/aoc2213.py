15fac6e433a04c35b9f0a99d05ec1ad5640ed8e2
dev <amal.imangulov@mcmakler.de>
2022-12-16 05:48:07 +0100

    aoc

--- aoc2022/aoc2213.py
+++ aoc2022/aoc2213.py
@@ -1,4 +1,4 @@
-test = True
+test = False
 if test:
     lines = """[1,1,3,1,1]
 [1,1,5,1,1]
@@ -27,10 +27,100 @@ else:
     with open('/Users/dev/Downloads/input (12).txt', 'r') as f:
         lines = f.readlines()
 
-def listcomp(l,r):
+#I stole Parser from Reddit, because I HATE WRITING PARSERS
+
+def parse(expr):
+    stack = []
+    current = []
+
+    parsing_number = False
+
+    for char in expr:
+        if parsing_number and not char.isdigit():
+            number = int("".join(current))
+            current = stack.pop()
+            current.append(number)
+            parsing_number = False
+
+        if char.isdigit():
+            if not parsing_number:
+                parsing_number = True
+                stack.append(current)
+                current = []
+            current.append(char)
+        elif char == "[":
+            stack.append(current)
+            current = []
+        elif char == "]":
+            tmp = current
+            current = stack.pop()
+            current.append(tmp)
 
 
+    return current[0]
+
+def compnum(l,r):
+    if l == r:
+        return None
+    return l < r
+
+def comparison(l,r):
+    iter = min(len(l),len(r))
+    for i in range(iter):
+        if (type(l[i])==type(int())) & (type(r[i])==type(int())):
+            c = compnum(l[i], r[i])
+            if None != c:
+                return c
+        elif (type(l[i])==type(list())) & (type(r[i])==type(list())):
+            c = comparison(l[i], r[i])
+            if None != c:
+                return c
+        elif (type(l[i])==type(int())) & (type(r[i])==type(list())):
+            c = comparison([l[i]],r[i])
+            if None != c:
+                return c
+        elif (type(l[i])==type(list())) & (type(r[i])==type(int())):
+            c = comparison(l[i],[r[i]])
+            if None != c:
+                return c
+    if len(l) != len(r):
+        return len(l) < len(r)
+    else:
+        return None
+
+
+
+p1s = 0
+sep1 = [[2]]
+sep2 = [[6]]
+packlist = [[[2]],[[6]]]
 for i in range(0,len(lines),3):
     l = lines[i]
     r = lines[i+1]
-    print(l,r)
+    pl = (parse(l))
+    pr = (parse(r))
+    cmpr = comparison(pl,pr)
+    if cmpr:
+        p1s+=(i//3 + 1)
+        packlist.append(pl)
+        packlist.append(pr)
+    else:
+        packlist.append(pr)
+        packlist.append(pl)
+
+#print(packlist)
+print(p1s)
+allissorted = False
+lp = len(packlist)
+while allissorted == False:
+    allissorted = True
+    for i in range(lp-1):
+        for k in range(i+1, lp):
+            if comparison(packlist[i],packlist[k])==False:
+                allissorted=False
+                packlist[i], packlist[k] = packlist[k], packlist[i]
+div1=(packlist.index(sep1))+1
+div2=(packlist.index(sep2))+1
+print(div1*div2)
+
+
